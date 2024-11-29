5cedb3bbcb9ac47a7175a387e2758a62c9488682
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-11-29 14:03:05 -0500

    keep api data in localstorage, add google fonts and styled comps

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Pages } from "./pages/Pages";
 function App() {
   return (
     <div className="App">
-      <h1>Recipe app</h1>
+      <h1 className="title">Recipe app</h1>
       <Pages />
     </div>
   );
