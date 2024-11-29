b2650a451fb20a827560ed2e62943e06517f87be
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-11-29 16:12:57 -0500

    create search component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import { Category } from "./components/Category";
 import { Pages } from "./pages/Pages";
+import { Search } from "./pages/Search";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Search />
         <Category />
         <Pages />
       </BrowserRouter>
---
+++ src/pages/Search.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import styled from "styled-components";
+
+export function Search() {
+  const [input, setInput] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <FormStyle>
+      <div>
+        <FaSearch />
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </div>
+    </FormStyle>
+  );
+}
+
+const FormStyle = styled.form`
+  margin: 0rem 20rem;
+  div {
+    position: relative;
+    width: 100%;
+  }
+  input {
+    border: none;
+    background: linear-gradient(35deg, #494949, #313131);
+    font-size: 1.5rem;
+    color: white;
+    padding: 1rem 3rem;
+    outline: none;
+    border-radius: 1rem;
+    width: 100%;
+  }
+
+  svg {
+    position: absolute;
+    top: 50%;
+    left: 0%;
+    transform: translate(100%, -50%);
+    color: white;
+  }
+`;
