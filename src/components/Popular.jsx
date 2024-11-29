03fc2c2f14bd2141a18e2efed33a746892de366e
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-11-29 13:04:55 -0500

    fetch data in Popular.jsx, load onto screen!

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,3 +1,29 @@
+import { useEffect, useState } from "react";
+
 export function Popular() {
-  return <div>Popular</div>;
+  const [popular, setPopular] = useState([]);
+  useEffect(() => {
+    getPopular();
+  }, []);
+  const getPopular = async () => {
+    const api = await fetch(
+      `https://api.spoonacular.com/recipes/random?apiKey=${
+        import.meta.env.VITE_REACT_APP_API_TOKEN
+      }&number=9`
+    );
+    const data = await api.json();
+    console.log(data.recipes);
+  };
+
+  return (
+    <div>
+      {popular.map((recipe) => {
+        return (
+          <div key={recipe.id}>
+            <p>{recipe.title}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
