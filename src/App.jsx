847e3c4f2ab1d53164842dece45082ff9c4de11c
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-11-29 15:01:33 -0500

    create category component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import { Category } from "./components/Category";
 import { Pages } from "./pages/Pages";
 
 function App() {
   return (
     <div className="App">
       <Pages />
+      <Category />
     </div>
   );
 }
---
+++ src/components/Category.jsx
@@ -0,0 +1,32 @@
+import { FaHamburger, FaPizzaSlice } from "react-icons/fa";
+import { GiChopsticks, GiNoodles } from "react-icons/gi";
+import { NavLink } from "react-router-dom";
+
+export function Category() {
+  return (
+    <List>
+      <NavLink to={"/cuisine/Italian"}>
+        <FaPizzaSlice />
+        <h4>Italian</h4>
+      </NavLink>
+      <NavLink to={"/cuisine/American"}>
+        <FaHamburger />
+        <h4>American</h4>
+      </NavLink>
+      <NavLink to={"/cuisine/Thai"}>
+        <GiNoodles />
+        <h4>Thai</h4>
+      </NavLink>
+      <NavLink to={"/cuisine/Japanese"}>
+        <GiChopsticks />
+        <h4>Japanese</h4>
+      </NavLink>
+    </List>
+  );
+}
+
+const List = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 2rem 0rem;
+`;
